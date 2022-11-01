1395e745786655a0758256e3e601134d83da4d9b
JCiaffa <107154269+JCiaffa@users.noreply.github.com>
2022-10-31 20:51:57 -0700

    Add files via upload

---
+++ Boxing-Unboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿List<object> list = new List<object>();
+        list.Add(7);
+        list.Add(28);
+        list.Add(-1);
+        list.Add(true);
+        list.Add("chair");
+
+int sum = 0;
+
+foreach(var i in list){
+    Console.WriteLine(i);
+}
+
+foreach(var i in list){
+    if(i is int){
+        sum += (int)i;
+    }
+}
+
+Console.WriteLine($"The sum of the integers in the list is: {sum}");
